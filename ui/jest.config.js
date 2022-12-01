e1e0d4c3fc885c6b1ae6df5b3b2920bdbeb59e19
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2022-12-01 15:06:21 +0100

    Fix CI for frontend.

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -1,7 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageReporters: ['lcov', 'text-summary'],
-  coverageDirectory: '<rootDir>',
-};
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageReporters: ['lcov', 'text-summary'],
+    coverageDirectory: '<rootDir>'
+}
