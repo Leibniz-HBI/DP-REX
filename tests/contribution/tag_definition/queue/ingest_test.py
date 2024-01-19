4d58f7dc410086619ed6b54c3b3fc1691bb4e51e
Moritz Fürneisen <m.fuerneisen@leibniz-hbi.de>
2024-01-19 15:50:35 +0100

    Allow uploads with empty lines.

--- tests/contribution/tag_definition/queue/ingest_test.py
+++ tests/contribution/tag_definition/queue/ingest_test.py
@@ -14,17 +14,34 @@ from vran.entity.models_django import Entity
 from vran.merge_request.models_django import TagMergeRequest
 from vran.tag.models_django import TagDefinition, TagInstance
 
+csv_cols = {
+    "names": ["name_0", "name_1"],
+    "verified": ["true", "false"],
+    "party": ["party_0", "party_1"],
+}
+
 
 @pytest.fixture
 def csv_mock():
     csv_mock = MagicMock()
-    csv_mock.return_value = pd.DataFrame(
-        {
-            "names": ["name_0", "name_1"],
-            "verified": ["true", "false"],
-            "party": ["party_0", "party_1"],
-        }
-    )
+
+    csv_mock.return_value = pd.DataFrame(csv_cols)
+    return csv_mock
+
+
+@pytest.fixture
+def csv_mock_with_empty_lines():
+    new_cols = {}
+    for name, vals in csv_cols.items():
+        new_vals = []
+        for val in vals:
+            new_vals.append(val)
+            new_vals.append("")
+            new_vals.append("\t\n")
+            new_vals.append(None)
+        new_cols[name] = new_vals
+    csv_mock = MagicMock()
+    csv_mock.return_value = pd.DataFrame(new_cols)
     return csv_mock
 
 
@@ -111,7 +128,6 @@ def test_ingest_columns_names_only(
 
 
 def get_tag_value_by_mr(entity_name, id_tag_persistent):
-
     origin_tag = TagDefinition.objects.filter(  # pylint: disable=no-member
         id_persistent=Subquery(
             TagMergeRequest.objects.filter(  # pylint: disable=no-member
@@ -205,3 +221,36 @@ def test_sets_error(party_contribution, party_tag_def):
     assert contribution.state == ContributionCandidate.COLUMNS_EXTRACTED
     assert contribution.error_msg == "Error during ingestion of assigned tags."
     assert contribution.error_trace == "Exception: error"
+
+
+def test_ingest_with_empty(
+    verified_tag_def,
+    verified_contribution,
+    csv_mock_with_empty_lines,
+):
+    contribution_other = verified_contribution.contribution_candidate
+    contribution_other.state = ContributionCandidate.COLUMNS_EXTRACTED
+    contribution_other.save()
+    with patch(
+        "vran.contribution.tag_definition.queue.util.read_csv",
+        csv_mock_with_empty_lines,
+    ):
+        ingest_values_from_csv(contribution_other.id_persistent)
+    persons = set(
+        Entity.objects.values_list(  # pylint: disable=no-member
+            "display_txt", flat=True
+        )
+    )
+    assert persons == {"name_0", "name_1"}
+    assert get_tag_value_by_mr("name_0", verified_tag_def.id_persistent) == "true"
+    assert get_tag_value_by_mr("name_1", verified_tag_def.id_persistent) == "false"
+    instances = TagInstance.objects.all()  # pylint: disable=no-member
+    assert len(instances) == 2
+    assert (
+        ContributionCandidate.objects.filter(  # pylint: disable=no-member
+            id_persistent=contribution_other.id_persistent
+        )
+        .get()
+        .state
+        == ContributionCandidate.VALUES_EXTRACTED
+    )
